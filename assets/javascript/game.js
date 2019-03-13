fe10177c3db9ff0e78d4ce52ab09a90f30589881
Xian <xian@Xians-Air.frontierlocal.net>
2019-03-12 21:56:05 -0700

    v1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,74 @@
 
-
 //string
 var animals = ["alpaca", "buffalo", "camel", "cat", "snake", 
                 "tiger", "rabbit", "horse", "sheep", "monkey",
-                "kangaroo", "giraffe", "dolphin", "goose", "crocodile"]
-
-var computerPick = pickWord();
-
-function pickWord(){
-    return animals[Math.floor(Math.random() * animals.length)];
-}
-
-var answerArray = [];
-for (var i = 0; i < computerPick.length; i++) {
- answerArray[i] = "_";
-}
-
-//make array to string and display it
-document.getElementById("wordPlace").textContent = answerArray.join(" ");
-
-
-var win = 0;
+                "kangaroo", "giraffe", "dolphin", "goose", "crocodile",
+                "flamingo", "elephent"]
+//var collection:
+var wins = 0;
 var losses = 0;
-var guessLeft = 10;
-var guessSoFar = "";
+var guessLeft = 15;
+var guessSoFar = [];
 
+var computerPick = "";
 var winsText = document.getElementById("wins");
 var lossesText = document.getElementById("losses");
 var guessLeftText = document.getElementById("guessLeft");
 var guessSoFarText = document.getElementById("guessSoFar");
-
-var computerPick = animals[Math.floor(Math.random() * animals.length)];
-
-
-
-document.getElementById("wordPlace").textContent = answerArray.join(" ");
-
+var wordPlaceText = document.getElementById("wordPlace");
 var remainingLetters = computerPick.length;
-//game loop
+var answerArray = [];
+
+//random pick word:
+function pickWord(){
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
+//display:
+function updateDisplayValue(){
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+    guessLeftText.textContent = guessLeft;
+    guessSoFarText.textContent = guessSoFar;
+    wordPlaceText.textContent = answerArray.join(" ");
+}
+
+//reset all the display:
+function reset(){
+    guessLeft = 15;
+    guessSoFar = [];
+    computerPick = pickWord();
+    answerArray = [];
+    for (var i = 0; i < computerPick.length; i++) {
+        answerArray[i] = "_";
+    }
+    remainingLetters = computerPick.length;
+    updateDisplayValue();
+}
+
+reset();
+//user press key:
 document.onkeyup = function(event){
     var userGuess = event.key.toLowerCase();
-if(userGuess.indexOf(computerPick)!=-1){
-    for (var j = 0; j < computerPick.length; j++){
-        if(computerPick[j] === userGuess){
-            answerArray[j] = userGuess;
-            remainingLetters--;
+    if(computerPick.indexOf(userGuess)!=-1){
+        for (var j = 0; j < computerPick.length; j++){
+            if(computerPick[j] === userGuess){
+                answerArray[j] = userGuess;
+                remainingLetters--;
+            }
+        }
+    }
+    guessSoFar += " " + userGuess;
+    guessLeft--;
+    //win
+    if(remainingLetters === 0){
+        wins++;
+        reset();
+    }
+    //loss
+    if(guessLeft === 0){
+        losses++;
+        reset();
+    }
+    updateDisplayValue();
 }
-}
-}
-}
-
-
-
-
-
